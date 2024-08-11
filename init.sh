39273e9f713fe570fb08c54e023ec1ba69b91091
Tim Vaulin <88082374+apeTim@users.noreply.github.com>
2024-08-11 09:27:58 +0300

    Add ssh protection

--- init.sh
+++ init.sh
@@ -16,3 +16,18 @@ echo "server {
 curl https://raw.githubusercontent.com/apeTim/active-probing-defence/main/index-$((1 + $RANDOM % 4)).html > /var/www/html/index.html
 certbot --nginx -d "$1" -d "www.$1" --register-unsafely-without-email
 systemctl restart nginx
+# Ssh security
+echo "Include /etc/ssh/sshd_config.d/*.conf
+
+Port $2
+PermitRootLogin yes
+ChallengeResponseAuthentication no
+UsePAM no
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+X11Forwarding yes
+PrintMotd no
+AcceptEnv LANG LC_*
+Subsystem       sftp    /usr/lib/openssh/sftp-server" > /etc/ssh/sshd_config
+echo "$3" >> authorized_keys
+service ssh restart
